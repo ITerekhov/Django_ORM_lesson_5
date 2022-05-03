db2ffa189d593949f5af62170f23ef2463d9d9ed
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-05-03 14:53:26 +0300

    Add field Flat.owner_pure_phone

--- property/models.py
+++ property/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+from phonenumber_field.modelfields import PhoneNumberField
+
+
 class User(User):
     name = models.CharField('Имя', max_length=200, unique=True)
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner_pure_phone = PhoneNumberField(verbose_name='Нормализированный номер владельца', blank=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -54,6 +57,7 @@ class Flat(models.Model):
         User, 
         verbose_name= 'Понравилось пользователям',
         related_name='Понравившиеся объекты+',
+        blank=True,
         )
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
